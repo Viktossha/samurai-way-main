ad57881f58b569e778b2121171436f6b83f43e7a
Vika <vikapetrova8@gmail.com>
2024-03-20 22:54:08 +0300

    Dialogs

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,10 +1,42 @@
 import React from 'react';
-import classes from './Dialogs.module.css'  //импортируем объект
+import classes from './Dialogs.module.css'
+import {NavLink} from "react-router-dom";  //импортируем объект
+
+type DialogItemType = {
+    name: string
+    id: string
+}
+const DialogItem = (props: DialogItemType) => {
+    return (
+        <div className={classes.dialog + ' ' + classes.active}>
+            <NavLink to={`/dialogs/${props.id}`}>{props.name}</NavLink>
+        </div>
+    )
+}
+
+type MessageType = {
+    message: string
+}
+const Message = (props: MessageType) => {
+  return (
+      <div className={classes.message}>{props.message}</div>
+  )
+}
 
 export const Dialogs = () => {
     return (
-        <div>
-            Dialogs
+        <div className={classes.dialogs}>
+            <div className={classes.dialogsItems}>
+                <DialogItem name='Varya' id='1'/>
+                <DialogItem name='Kristina' id='2'/>
+                <DialogItem name='Misha' id='3'/>
+                <DialogItem name='Alina' id='4'/>
+            </div>
+            <div className={classes.messages}>
+                <Message message='Hello'/>
+                <Message message='Hi'/>
+                <Message message='How are you?'/>
+            </div>
         </div>
     );
 };
