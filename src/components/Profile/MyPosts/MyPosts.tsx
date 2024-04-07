c22d2a893d431cb694f09de0e01d9beabda0d349
Vika <vikapetrova8@gmail.com>
2024-04-07 22:43:35 +0300

    MyPosts

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -4,16 +4,18 @@ import {Post} from "./Post/Post";
 
 export const MyPosts = () => {
     return (
+        <div className={classes.postsBlock}>
+            <h3>My posts</h3>
             <div>
-                my posts
+                <div><textarea></textarea></div>
                 <div>
-                    <textarea></textarea>
                     <button>Add post</button>
                 </div>
-                <div className={classes.posts}>
-                    <Post message='Hi, how are you' likesCounts='15'/>
-                    <Post message="It's my first post" likesCounts='20'/>
-                </div>
             </div>
+            <div className={classes.posts}>
+                <Post message='Hi, how are you' likesCounts='15'/>
+                <Post message="It's my first post" likesCounts='20'/>
+            </div>
+        </div>
     );
 };
