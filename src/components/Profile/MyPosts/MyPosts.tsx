105cf124adcc49405d58630a021f052535ef0c65
Vika <vikapetrova8@gmail.com>
2024-05-08 20:27:51 +0300

    addPost handler

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {LegacyRef, RefObject} from 'react';
 import classes from './MyPosts.module.css'
 import {Post} from "./Post/Post";
 
@@ -16,13 +16,19 @@ export const MyPosts = (props: MyPostsPropsType) => {
 
     let postsElements = props.posts.map(p => <Post message={p.message} likesCounts={p.likesCounts}/>)
 
+    let newPostElement:  RefObject<HTMLTextAreaElement>  = React.createRef()
+    let addPost = () => {
+        let text = newPostElement.current?.value
+        alert(text)
+    }
+
     return (
         <div className={classes.postsBlock}>
             <h3>My posts</h3>
             <div>
-                <div><textarea></textarea></div>
+                <div><textarea ref={newPostElement}></textarea></div>
                 <div>
-                    <button>Add post</button>
+                    <button onClick={addPost}>Add post</button>
                 </div>
             </div>
             <div className={classes.posts}>
